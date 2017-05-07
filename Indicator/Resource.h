26a0794b2285ec10cbb2867a1f3063f9618b2969
lee <litao1009@gmail.com>
2017-05-08 02:02:13 +0800

    -update

--- Indicator/Resource.h
+++ Indicator/Resource.h
@@ -11,13 +11,14 @@
 #define IDC_TXT_HSS                     1002
 #define ID_IMPORT                       32772
 #define ID_MODIFY_PARAM                 32774
+#define ID_ACCELERATOR32778             32778
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        311
-#define _APS_NEXT_COMMAND_VALUE         32778
+#define _APS_NEXT_COMMAND_VALUE         32780
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
